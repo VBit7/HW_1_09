0e6837e999815f656c9997cbf761aea636f85238
Vitalii Bielimov <vbit2000@gmail.com>
2023-11-10 14:35:40 +0200

    the structure of the module is defined

--- main.py
+++ main.py
@@ -3,79 +3,105 @@ def input_error(func):
         try:
             return func(*args, **kwargs)
         except KeyError:
-            return "Enter user name"
+            return "KeyError"
         except ValueError:
-            return "Give me name and phone please"
+            return "ValueError"
         except IndexError:
-            return "Invalid input. Please try again."
-
+            return "IndexError"
+    
     return wrapper
+
 
 contacts = {}
 
-@input_error
-def add_contact(params):
-    name, phone = params
-    contacts[name] = phone
-    return f"Contact {name} with phone {phone} added."
+
+# "hello"      - відповідає у консоль "How can I help you?
+# "add ..."    - зберігає у словнику новий контакт. Замість ... - ім'я та номер телефону, обов'язково через пробіл.
+# "change ..." - зберігає в пам'яті новий номер телефону існуючого контакту. Замість ... - ім'я та номер телефону, обов'язково через пробіл.
+# "phone ...." - виводить у консоль номер телефону для зазначеного контакту. Замість ... - ім'я контакту, чий номер треба показати.
+# "show all"   - виводить всі збереженні контакти з номерами телефонів у консоль.
+# "good bye", "close", "exit" - по будь-якій з цих команд бот завершує свою роботу.
+
 
 @input_error
-def change_phone(params):
-    name, phone = params
-    if name in contacts:
-        contacts[name] = phone
-        return f"Phone number updated for {name}. New phone number: {phone}"
-    else:
-        raise KeyError
+def hello(params):
+    return "How can I help you?"
+
 
 @input_error
-def show_phone(name):
-    if name in contacts:
-        return f"Phone number for {name}: {contacts[name]}"
-    else:
-        raise KeyError
+def add(params):
+    return "add"
 
-def show_all_contacts():
-    if not contacts:
-        return "No contacts found."
-    result = "Contacts:\n"
-    for name, phone in contacts.items():
-        result += f"{name}: {phone}\n"
-    return result
 
-def handle_command(command):
-    command = command.lower()
-    if command == "hello":
-        return "How can I help you?"
-    elif command.startswith("add "):
-        params = command[4:].split(" ")
-        if len(params) == 2:
-            return add_contact(params)
-        else:
-            raise ValueError
-    elif command.startswith("change "):
-        params = command[7:].split(" ")
-        if len(params) == 2:
-            return change_phone(params)
-        else:
-            raise ValueError
-    elif command.startswith("phone "):
-        name = command[6:]
-        return show_phone(name)
-    elif command == "show all":
-        return show_all_contacts()
-    elif command in ["good bye", "close", "exit"]:
-        return "Good bye!"
-    else:
-        raise ValueError
+@input_error
+def change(params):
+    return "change"
+
+
+@input_error
+def phone(params):
+    return "phone"
+
+
+@input_error
+def show(params):
+    return "show all"
+
+
+def help(params):
+    help_msg = """
+        "hello" - Responds in the console with "How can I help you?"
+        
+        "add ..." - Saves a new contact in the dictionary.
+            Replace "..." with the name and phone number, separated by a space.
+        
+        "change ..." - Updates the phone number of an existing contact in memory.
+            Replace "..." with the name and new phone number, separated by a space.
+        
+        "phone ...." - Outputs the phone number for the specified contact to the console.
+            Replace "..." with the name of the contact whose number needs to be shown.
+        
+        "show all" - Displays all saved contacts with phone numbers in the console.
+        
+        "good bye", "close", "exit" - Any of these commands will terminate the bot's operation.
+    """
+    return help_msg
+
+
+commands = {
+    "hello": hello,
+    "add": add,
+    "change": change,
+    "phone": phone,
+    "show": show,
+    "help": help,
+}
+
 
 def main():
+    # print("Hello!")
     while True:
-        user_input = input("Enter a command: ")
-        if user_input == ".":
+        user_input = input("Enter a command: ").lower().strip()
+
+        if user_input in ["good bye", "close", "exit", "."]:
             break
-        response = handle_command(user_input)
-        print(response)
+
+        params = user_input.split(" ")
+        
+        if params[0] in commands:
+            print(user_input)
+            response = commands[params[0]](params[1:])
+            print(response)
+        else:
+            print("Unknown command. Type 'help' to see available commands.")
+    
+    print("Good bye!")
+
+    return None
+
 
 if __name__ == "__main__":
     main()
+
+
+
